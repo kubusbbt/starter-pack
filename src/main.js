b5dc09b7f6c24ce2631762f266bb1c4a37a4a47b
Kuba <kubusbbt@gmail.com>
2018-07-10 15:05:30 +0200

    clean & add bootstrap, jquery

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import $ from 'jquery'
-// import '../node_modules/bootstrap';
+import $ from 'jquery';
+import '../node_modules/popper.js/dist/popper.min.js';
+import '../node_modules/bootstrap';
 
 
 let test = 'jQuery works!';
