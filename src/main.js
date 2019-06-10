56e04c0c049c1783cd4ab5a64dd9bdec6ab6770c
cod3r <kubusbbt@gmail.com>
2019-06-10 13:29:24 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
 import $ from 'jquery'
 import '../node_modules/popper.js/dist/popper.min.js';
 import '../node_modules/bootstrap';
-import navComponent from './scripts/nav';
+import navComponent from './components/nav';
+import './components/dev';
 
 navComponent();
-
-let test = 'jQuery works!';
-
-$(document).ready(function(){
-	console.log( test );
-})
