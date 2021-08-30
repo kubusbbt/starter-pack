ccf93a89bdaefd5fa1669c504fcc344015b8ea4c
kubusbbt <kubusbbt@gmail.com>
2021-08-30 16:27:01 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
 // import $ from 'jquery'
 // import '../node_modules/popper.js/dist/popper.min.js';
 // import '../node_modules/bootstrap';
-// import navComponent from './components/nav';
 // import './components/dev';
 // import Vue from '../node_modules/vue/dist/vue';
 
 // import test from './components/test.vue';
-
-// navComponent();
-
 
 // Vue.component('test', test)
 
